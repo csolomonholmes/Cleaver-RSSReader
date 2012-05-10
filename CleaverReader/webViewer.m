6f1b0613d1abebb1525ff291f83db1d372ac9013
Randy McMillan <randymcmillan@DeepSpaceMacBookPro>
2012-05-10 18:09:22 -0400

    ..

--- CleaverReader/webViewer.m
+++ CleaverReader/webViewer.m
@@ -106,7 +106,7 @@
 // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
-    return YES;
+    return NO;//(interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 
